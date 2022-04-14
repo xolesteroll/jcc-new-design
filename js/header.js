793ff8a9b87a5fe245cbf6429b4b3d084a2dd8d5
Anton <Iarovoi>
2022-04-14 10:38:55 +0300

    responsiveness for all pages except homepage

--- js/header.js
+++ js/header.js
@@ -2,11 +2,13 @@ $(document).ready(function () {
 
 
     $(".header-nav__toggle-btn").click(function () {
+        $(".mobile-nav").removeClass("hidden");
         $(".mobile-nav").addClass("active");
     });
 
     $(".mobile-nav__close-btn").click(function () {
         $(".mobile-nav").removeClass("active");
+        $(".mobile-nav").addClass("hidden");
     });
 
 });
